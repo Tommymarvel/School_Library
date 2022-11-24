addedbfcb8dc59c7ec89e132d51789732b5513a1
Tommymarvel <ibironketomiwa4@gmail.com>
2022-11-24 01:00:47 +0100

    Decorate classes

---
+++ result.rb
@@ -0,0 +1,10 @@
+require './capitalizedecorator'
+require './trimmerdecorator'
+require './person'
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+capitalized_trimmed_person.correct_name
